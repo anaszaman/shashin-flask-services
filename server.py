11e3eb186729311872da172e694b2164bd16c744
Anas Zaman <anaszaman.az@gmail.com>
2015-12-11 17:34:51 -0600

    Initial Python server functionality

--- server.py
+++ server.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from boto.s3.key import Key
+import boto
+import boto.s3.connection
+import uuid
+
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+
+def hello(data):
+	try:
+		connection = boto.connect_s3()
+		bucket_name = "shashin-test"
+		bucket = connection.get_bucket(bucket_name)
+		key = Key(bucket)
+		guid = uuid.uuid4()
+		key.key = guid
+		key.set_contents_from_string(data)
+		key.make_public()
+		print "Success"
+	except Exception, exception:
+		return exception
 
 if __name__ == "__main__":
     app.run()
+    
